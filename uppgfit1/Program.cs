d870dd683bfe2c1f66984539d192ffb08e15afb0
Alexsept177 <alan03002@utb.vaxjo.se>
2022-09-30 11:06:43 +0200

    Add project files.

---
+++ uppgfit1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uppgfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange längden på låten för att se om den får spelas på radion(ange med komma, exempel: 1,23)");
+            double tid = double.Parse(Console.ReadLine());
+
+
+            if (tid >= 2.45 && tid <= 4.20)
+            {
+                Console.WriteLine("Din låt kan spelas på radion");
+            }
+            else
+            {
+                
+                Console.WriteLine("Din låt är för lång eller kort för att kunna spelas på radion");
+            }
+        }
+    }
+}
